f00a98485e1442dbb4c76afc56fcc9911207c29a
Xavier Pillons (MSFT) <xpillons@microsoft.com>
2024-04-23 20:06:12 +0200

    add rename host

---
+++ specs/default/cluster-init/files/almalinux/rename_host.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# Stop NetworkManager overwriting /etc/resolv.conf
+cat > /etc/NetworkManager/conf.d/90-dns-none.conf << EOF
+[main]
+dns=none
+EOF
+
+function enforce_hostname() {
+  local system_hostname=$1
+  local target_hostname=$2
+
+  # ensure the correct hostname (update if necessary)
+  echo "ensure the correct hostname (update if necessary)"
+
+  if [ "$system_hostname" != "$target_hostname" ]; then
+    logger -s "Warning: incorrect hostname ($system_hostname), it should be $target_hostname, updating"
+    hostname $target_hostname
+  fi
+  if grep -i $system_hostname /etc/hosts; then
+    logger -s "Warning: incorrect hostname ($system_hostname) in /etc/hosts, updating"
+    sed -i "s/$system_hostname/$target_hostname/ig" /etc/hosts
+  fi
+  etc_hostname=$(</etc/hostname)
+  if [ "$etc_hostname" != "$target_hostname" ]; then
+    logger -s "Warning: incorrect /etc/hostname ($etc_hostname), it should be $target_hostname, updating"
+    echo $target_hostname > /etc/hostname
+  fi
+  eth0_hostname=$(grep DHCP_HOSTNAME /etc/sysconfig/network-scripts/ifcfg-eth0 | cut -d'=' -f2)
+  if [ "$eth0_hostname" != "" -a "$eth0_hostname" != "$target_hostname" ]; then
+    logger -s "Warning: incorrect DHCP_HOSTNAME in /etc/sysconfig/network-scripts/ifcfg-eth0 ($etc_hostname), it should be $target_hostname, updating"
+    sed -i "s/^DHCP_HOSTNAME=.*\$/DHCP_HOSTNAME=$target_hostname/g" /etc/sysconfig/network-scripts/ifcfg-eth0
+    systemctl restart NetworkManager
+  fi
+}
---
+++ specs/default/cluster-init/scripts/01-rename_host.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+source "$script_dir/../files/azhop-helpers.sh" 
+read_os
+
+source $script_dir/../files/$os_release/rename_host.sh
+
+function rename_host() {
+  delay=15
+  n=1
+  max_retry=3
+
+  standalone_dns=$(jetpack config cyclecloud.hosts.standalone_dns.enabled | tr '[:upper:]' '[:lower:]')
+  if [[ $standalone_dns != "true" ]]; then
+    while true; do
+      current_hostname=$(hostname)
+      target_hostname=$(jetpack config cyclecloud.node.name)
+
+      if [[ $n -le $max_retry ]]; then
+        if [ "$current_hostname" != "$target_hostname" ]; then
+          logger -s "hostname not renamed -  Attempt $n/$max_retry:"
+          enforce_hostname $current_hostname $target_hostname
+          sleep $delay
+        else
+          logger -s "hostname successfully renamed"
+          break
+        fi
+        ((n++))
+      else
+        logger -s "Failed to rename host after $n attempts."
+        exit 1
+      fi
+    done
+  fi
+}
+
+rename_host
