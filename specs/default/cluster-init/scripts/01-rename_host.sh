00d74ec31430551a6bc84e56c874c5509e6302cb
Xavier Pillons (MSFT) <xpillons@microsoft.com>
2024-04-23 20:19:49 +0200

    fix helper

--- specs/default/cluster-init/scripts/01-rename_host.sh
+++ specs/default/cluster-init/scripts/01-rename_host.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-source "$script_dir/../files/azhop-helpers.sh" 
+source "$script_dir/../files/helpers.sh" 
 read_os
 
 source $script_dir/../files/$os_release/rename_host.sh
