2359b398374c926f6ce8c8308e6814a65ecd2cee
Xavier Pillons (MSFT) <xpillons@microsoft.com>
2024-04-24 16:37:09 +0200

    improve node renaming

--- specs/default/cluster-init/scripts/01-rename_host.sh
+++ specs/default/cluster-init/scripts/01-rename_host.sh
@@ -14,10 +14,11 @@ function rename_host() {
   if [[ $standalone_dns != "true" ]]; then
     while true; do
       current_hostname=$(hostname | tr '[:upper:]' '[:lower:]')
+      hostname_in_hosts=$(getent hosts $(ifconfig eth0 | grep "inet " | xargs) | xargs | cut -d ' ' -f2 | tr '[:upper:]' '[:lower:]')
       target_hostname=$(jetpack config cyclecloud.node.name | tr '[:upper:]' '[:lower:]')
 
       if [[ $n -le $max_retry ]]; then
-        if [ "$current_hostname" != "$target_hostname" ]; then
+        if [[ "$current_hostname" != "$target_hostname" || "$target_hostname" != "$hostname_in_hosts" ]]; then
           logger -s "hostname not renamed -  Attempt $n/$max_retry:"
           enforce_hostname $current_hostname $target_hostname
           sleep $delay
