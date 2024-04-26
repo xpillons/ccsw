b9f8a4fcb036f3b39100be2c1917457e1c3980f6
Xavier Pillons (MSFT) <xpillons@microsoft.com>
2024-04-26 11:57:01 +0200

    fix inner loop

--- specs/default/cluster-init/scripts/01-rename_host.sh
+++ specs/default/cluster-init/scripts/01-rename_host.sh
@@ -13,44 +13,47 @@ function check_host_renaming() {
   standalone_dns=$(jetpack config cyclecloud.hosts.standalone_dns.enabled | tr '[:upper:]' '[:lower:]')
   if [[ $standalone_dns != "true" ]]; then
     while true; do
+      # Get current hostname
       current_hostname=$(hostname | tr '[:upper:]' '[:lower:]')
+      # Get hostname associated with the IP address
       hostname_in_hosts=$(getent hosts $(ifconfig eth0 | grep "inet " | xargs) | xargs | cut -d ' ' -f2 | tr '[:upper:]' '[:lower:]')
+      # Get the target hostname
       target_hostname=$(jetpack config cyclecloud.node.name | tr '[:upper:]' '[:lower:]')
       logger -s "Current hostname: $current_hostname"
-      logger -s "Hostname in /etc/hosts: $hostname_in_hosts"
+      logger -s "Hostname returned by 'getent hosts': $hostname_in_hosts"
       logger -s "Target hostname: $target_hostname"
       if [[ $n -le $max_retry ]]; then
         if [[ "$current_hostname" != "$target_hostname" || "$target_hostname" != "$hostname_in_hosts" ]]; then
           logger -s "$target_hostname not resolvable -  Attempt $n/$max_retry:"
           enforce_hostname $current_hostname $target_hostname
           sleep $delay
+          ((n++))
         else
-          logger -s "hostname successfully renamed and resolved"
+          logger -s "hostname successfully renamed to $target_hostname"
           break
         fi
+      else
+        logger -s "Failed to rename host $target_hostname after $max_retry attempts."
+        exit 1
+      fi
+    done
+    # Check if the new hostname is resolvable and loop until it is or max_retry is reached
+    n=1
+    max_retry=3
+    while true; do
+      logger -s "Checking if $target_hostname is resolvable"
+      nslookup $target_hostname
+      if [ $? -eq 0 ]; then
+        break
+      fi
+      if [[ $n -le $max_retry ]]; then
+        logger -s "$target_hostname not resolvable -  Attempt $n/$max_retry:"
+        sleep $delay
         ((n++))
       else
         logger -s "Failed to resolved host $target_hostname after $max_retry attempts."
         exit 1
       fi
-      # Check if the new hostname is resolvable and loop until it is or max_retry is reached
-      n=1
-      max_retry=3
-      while true; do
-        logger -s "Checking if $target_hostname is resolvable"
-        nslookup $target_hostname
-        if [ $? -eq 0 ]; then
-          break
-        fi
-        if [[ $n -le $max_retry ]]; then
-          logger -s "$target_hostname not resolvable -  Attempt $n/$max_retry:"
-          sleep $delay
-          ((n++))
-        else
-          logger -s "Failed to resolved host $target_hostname after $max_retry attempts."
-          exit 1
-        fi
-      done
     done
   fi
 }
