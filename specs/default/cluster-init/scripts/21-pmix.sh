eae2e4de1eacf6cc9807270994a96a17922386a0
Xavier Pillons (MSFT) <xpillons@microsoft.com>
2024-04-30 15:31:37 +0200

    build function

--- specs/default/cluster-init/scripts/21-pmix.sh
+++ specs/default/cluster-init/scripts/21-pmix.sh
@@ -1,24 +1,9 @@
 #!/bin/bash
 set -e
-
-# Install PMIx if not present in the image
-PMIX_DIR=/opt/pmix/v4
 PMIX_VERSION=4.2.9
-# if the $PMIX_DIR directory is not present, then install PMIx
-if [ ! -d $PMIX_DIR ]; then
-    os_release=$(cat /etc/os-release | grep "^ID\=" | cut -d'=' -f 2 | xargs)
-    logger -s "Installing PMIx dependencies for $os_release"
-    case $os_release in
-        rhel|almalinux)
-            dnf config-manager --set-enabled powertools
-            dnf -y install autoconf flex libevent-devel git hwloc-devel
-            ;;
-        ubuntu|debian)
-            apt-get update
-            #apt-get install -y git libevent-dev libhwloc-dev autoconf flex make gcc libxml2
-            apt-get install -y libevent-dev libhwloc-dev
-            ;;
-    esac
+PMIX_DIR=/opt/pmix/$PMIX_VERSION
+
+function build_pmix() {
     # Build PMIx
     logger -s "Build PMIx"
     cd /mnt/scratch
@@ -38,10 +23,28 @@ if [ ! -d $PMIX_DIR ]; then
 	rm -rf pmix-${PMIX_VERSION}
     rm pmix-$PMIX_VERSION.tar.gz
     logger -s "PMIx Sucessfully Installed"
+}
 
-    ln -s $PMIX_DIR/lib/libpmix.so /usr/lib/libpmix.so
+# Install PMIx if not present in the image
+# if the $PMIX_DIR directory is not present, then install PMIx
+if [ ! -d $PMIX_DIR ]; then
+    os_release=$(cat /etc/os-release | grep "^ID\=" | cut -d'=' -f 2 | xargs)
+    case $os_release in
+        rhel|almalinux)
+            logger -s "Installing PMIx $PMIX_VERSION for $os_release"
+            yum -y install pmix-$PMIX_VERSION-1.el8
+            ;;
+        ubuntu|debian)
+            logger -s "Installing PMIx dependencies for $os_release"
+            apt-get update
+            #apt-get install -y git libevent-dev libhwloc-dev autoconf flex make gcc libxml2
+            apt-get install -y libevent-dev libhwloc-dev
+            build_pmix
+            ln -s $PMIX_DIR/lib/libpmix.so /usr/lib/libpmix.so
+            ;;
+    esac
+
     systemctl restart slurmd
-    systemctl status slurmd
 fi
 
 # Exit if Enroot is not in the image
